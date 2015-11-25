45afd3ebc0738a065312f28a6fceb5b278cf36ab
Dragos Rosu <r.dragosh@yahoo.com>
2015-11-25 18:58:41 +0000

    feat: refactor actions

--- ChewySuperstar/ChewySuperstar.Main/DefaultAction.cs
+++ ChewySuperstar/ChewySuperstar.Main/DefaultAction.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using InnerSpaceAPI;
 
 namespace ChewySuperstar.Main
 {
-    public class DefaultAction : IEVEAction
+    public class DefaultAction : EVEAction
     {
-        private int executeCount;
-
-        public void Execute()
+        protected override void ExecuteAction()
         {
-            InnerSpace.Echo(string.Format("{0:HH:mm:ss} Execute Count: {1}", DateTime.Now, executeCount++));
         }
     }
 }
---
+++ ChewySuperstar/ChewySuperstar.Main/EVEAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using InnerSpaceAPI;
+
+namespace ChewySuperstar.Main
+{
+    public abstract class EVEAction : IEVEAction
+    {
+        private int executeCount;
+
+        protected virtual string ActionName { get { return GetType().Name; } }
+
+        public void Execute()
+        {
+            ExecuteAction();
+            LogExecuteCount();
+        }
+
+        private void LogExecuteCount()
+        {
+            InnerSpace.Echo(string.Format("{0:HH:mm:ss} {1} Execute Count: {2}", DateTime.Now, ActionName, ++executeCount));
+        }
+
+        protected abstract void ExecuteAction();
+    }
+}
